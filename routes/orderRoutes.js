19e00027887a7bfbc647d7cafaab0561139bdefd
Ebuka <ebukaobiora10@gmail.com>
2025-02-19 00:39:30 +0100

    aggregate endpoints

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/', orderController.createOrder);
 router.get('/', orderController.getOrders);
+router.get('/sales-report', orderController.getSalesReport);
 
 module.exports = router;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post('/', productController.createProduct);
 router.get('/', productController.getProducts);
 router.get('/:id', productController.getProductById);
+router.get('/category-analysis', productController.getCategoryAnalysis);
+router.get('/product-stats', productController.getProductStatistics)
 
 module.exports = router;
